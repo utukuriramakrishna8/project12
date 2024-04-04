c446e7a4670b6cf36e3494e1dbeaf74b442cb941
utukuriramakrishna8 <utukuriramakrishna8@gmail.com>
2024-04-04 22:10:05 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def actor(request):
+    return render(request,'actor.html')
+def actor2(request):
+    return render(request,'actor2.html')
+
+# Create your views here.
